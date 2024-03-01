8bfdb775d036c62544b5f5266c90f1cd8ab23f9d
DanielRivasMD <danielrivasmd@gmail.com>
2024-03-01 05:51:14 -0600

    update cmd root

--- cmd/root.go
+++ cmd/root.go
@@ -21,7 +21,6 @@ import (
 	"log"
 	"os"
 
-	"github.com/atrox/homedir"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
@@ -29,16 +28,13 @@ import (
 )
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-// Mbombo, also called Bumba, is the creator god in the religion
-// and mythology of the Kuba people of Central Africa in the area
-// that is now known as Democratic Republic of the Congo.
-// In the Mbombo creation myth, Mbombo was a giant in form and
-// white in color. The myth describes the creation of the universe
-// from nothing.
-////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // declarations
-var ()
+var (
+	out string
+	files []string
+	path string
+)
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -112,7 +108,7 @@ func bindFlags(κ *cobra.Command, ω *viper.Viper) {
 		if !σ.Changed && ω.IsSet(σ.Name) {
 			ν := ω.Get(σ.Name)
 			κ.Flags().Set(σ.Name, fmt.Sprintf("%v", ν))
-		// apply viper config value flag
+			// apply viper config value flag
 		}
 	})
 }
@@ -123,29 +119,12 @@ func bindFlags(κ *cobra.Command, ω *viper.Viper) {
 func init() {
 
 	// persistent flags
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// fileExist checks if a file exists and is not a directory before try using it to prevent further errors
-func fileExist(ƒ string) bool {
-	info, ε := os.Stat(ƒ)
-	if os.IsNotExist(ε) {
-		return false
-	}
-	return !info.IsDir()
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// find home directory
-func findHome() string {
-	Λ, ε := homedir.Dir()
-	if ε != nil {
-		log.Fatal(ε)
-		os.Exit(1)
-	}
-	return Λ
+	rootCmd.PersistentFlags().StringVarP(&path, "path", "p", "", "Where are the itmes to be forged?")
+	rootCmd.MarkFlagRequired("path")
+	rootCmd.PersistentFlags().StringArrayVarP(&files, "files", "f", []string{}, "These items will create...")
+	rootCmd.MarkFlagRequired("files")
+	rootCmd.PersistentFlags().StringVarP(&out, "out", "o", "", "Where will the forge be delivered?")
+	rootCmd.MarkFlagRequired("out")
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
