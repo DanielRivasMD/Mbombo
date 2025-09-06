b48302e3a9ea57e4140c5b9dbb5bd3fcc32c443e
Daniel Rivas <danielrivasmd@gmail.com>
2025-09-06 11:21:58 +0200

    update format

--- cmd/root.go
+++ cmd/root.go
@@ -16,6 +16,8 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 package cmd
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 import (
 	"github.com/DanielRivasMD/horus"
 	"github.com/spf13/cobra"
