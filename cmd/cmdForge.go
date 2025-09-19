9f13045a1e34f87a4f5f9c031138a11586bd796e
Daniel Rivas <danielrivasmd@gmail.com>
2025-09-19 09:27:48 +0200

    relocate cat files function forge cmd

--- cmd/cmdForge.go
+++ cmd/cmdForge.go
@@ -122,3 +122,90 @@ func runForge(cmd *cobra.Command, args []string) {
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// catFiles concatenates files specified in options.Files into the out file (options.OutPath)
+// It checks if the out file exists using FileExist (with an inline no-action) & removes it if it does
+// All errors are propagated using horus.PropagateErr
+func catFiles(options forgeOptions) error {
+	op := "cat"
+
+	exists, err := domovoi.FileExist(options.outPath, func(path string) (bool, error) {
+		return false, nil
+	}, verbose)
+	if err != nil {
+		return horus.PropagateErr(
+			op,
+			"file_exist_error",
+			"failed to check out file existence",
+			err,
+			map[string]any{"outpath": options.outPath},
+		)
+	}
+
+	if exists {
+		if err := os.Remove(options.outPath); err != nil {
+			return horus.PropagateErr(
+				op,
+				"file_remove_error",
+				"failed to remove existing out file",
+				err,
+				map[string]any{"outpath": options.outPath},
+			)
+		}
+	}
+
+	fwrite, err := os.OpenFile(options.outPath, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		return horus.PropagateErr(
+			op,
+			"open_file_error",
+			"failed to open out file",
+			err,
+			map[string]any{"outpath": options.outPath},
+		)
+	}
+	defer fwrite.Close()
+
+	writer := bufio.NewWriter(fwrite)
+
+	for _, file := range options.files {
+		srcPath := options.inPath + "/" + file
+		fread, err := os.ReadFile(srcPath)
+		if err != nil {
+			return horus.PropagateErr(
+				op,
+				"read_source_error",
+				"failed to read source file",
+				err,
+				map[string]any{"source": srcPath},
+			)
+		}
+
+		// Apply replacements before writing
+		content := applyReplacements(string(fread), replacePairs)
+
+		if _, err := writer.WriteString(content + "\n"); err != nil {
+			return horus.PropagateErr(
+				op,
+				"write_error",
+				"failed to write to out file",
+				err,
+				map[string]any{"source": srcPath},
+			)
+		}
+	}
+
+	if err := writer.Flush(); err != nil {
+		return horus.PropagateErr(
+			op,
+			"flush_error",
+			"failed to flush writer",
+			err,
+			map[string]any{"outpath": options.outPath},
+		)
+	}
+
+	return nil
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
