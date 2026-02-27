1fb140a880a3cd399fbb76efaf861033f3cd2e93
Daniel Rivas <danielrivasmd@gmail.com>
2026-02-27 11:41:35 +0100

    add more color docs util

--- cmd/utilDocs.go
+++ cmd/utilDocs.go
@@ -1,5 +1,19 @@
-////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+Copyright © 2024 Daniel Rivas <danielrivasmd@gmail.com>
 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 package cmd
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -85,24 +99,33 @@ func styleLongHelp(text string) string {
 			lines[i] = chalk.Dim.TextStyle(chalk.Cyan.Color(line))
 		}
 	}
-
 	return strings.Join(lines, "\n")
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+func styleDescription(text string) string {
+	if text == "" {
+		return ""
+	}
+	return chalk.Cyan.Color(chalk.Dim.TextStyle(text))
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 func authorHeader() string {
 	return chalk.Bold.TextStyle(
-		chalk.Green.Color(NAME),
+		chalk.Green.Color("Daniel Rivas"),
 	) + " " +
 		chalk.Dim.TextStyle(
-			chalk.Italic.TextStyle(EMAIL),
+			chalk.Italic.TextStyle("<danielrivasmd@gmail.com>"),
 		)
 }
 
 func formatLongHelp(help string) string {
 	if help != "" {
-		return authorHeader() + "\n\n" + help
+		styledDesc := styleDescription(help)
+		return authorHeader() + "\n\n" + styledDesc
 	}
 	return authorHeader()
 }
