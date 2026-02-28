502ce4e010a1e033db34106cb5297403231ae2f7
Daniel Rivas <danielrivasmd@gmail.com>
2026-02-28 11:09:35 +0100

    update identity cmd

--- cmd/cmdIdentity.go
+++ cmd/cmdIdentity.go
@@ -40,7 +40,16 @@ that is now known as Democratic Republic of the Congo
 
 In the Mbombo creation myth, Mbombo was a giant in form and
 white in color. The myth describes the creation of the universe
-from nothing`)
+from nothing
+
+
+Role: Mbombo is considered a creator god in Bushongo mythology.
+
+Creation story: According to legend, in the beginning, there was only darkness and water, and Mbombo was the only being. He was a giant, pale god who eventually felt pain in his stomach and vomited up the sun, moon, stars, and then the Earth itself, including animals and people.
+
+Symbolism: His creation of the world through vomiting is unique and symbolic, often interpreted as a metaphor for creative force through suffering or sacrifice.
+
+Assistants: After creation, Mbombo delegated tasks to his sons and some of the first humans and animals to help finish shaping the world.`)
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
